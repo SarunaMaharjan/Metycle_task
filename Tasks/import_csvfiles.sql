e0f8088dc991d86c0c26e25ebdabda0ce26697f8
Saruna Maharjan <uniquejane10@gmail.com>
2024-07-02 11:54:11 +0200

    Commit changes

---
+++ Tasks/import_csvfiles.sql
@@ -0,0 +1,3 @@
+COPY credits FROM 'C:/Users/Saruna/Desktop/Task files/movies/credits.csv' DELIMITER ',' CSV HEADER;
+COPY ratings FROM 'C:/Users/Saruna/Desktop/Task files/movies/ratings.csv' DELIMITER ',' CSV HEADER;
+COPY titles FROM 'C:/Users/Saruna/Desktop/Task files/movies/titles.csv' DELIMITER ',' CSV HEADER;
