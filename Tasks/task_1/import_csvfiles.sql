6ae6abc6fdd74a9ffb4d6634e46dce93280f01b6
Saruna Maharjan <uniquejane10@gmail.com>
2024-07-02 11:55:49 +0200

    Commit changes

--- Tasks/task_1/import_csvfiles.sql
+++ Tasks/task_1/import_csvfiles.sql
@@ -1,3 +1,5 @@
+--sql script executed in pgadmin and csv files were pulled here.
+
 COPY credits FROM 'C:/Users/Saruna/Desktop/Task files/movies/credits.csv' DELIMITER ',' CSV HEADER;
 COPY ratings FROM 'C:/Users/Saruna/Desktop/Task files/movies/ratings.csv' DELIMITER ',' CSV HEADER;
 COPY titles FROM 'C:/Users/Saruna/Desktop/Task files/movies/titles.csv' DELIMITER ',' CSV HEADER;
